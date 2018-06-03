629b17792da2a246ecb116f1fb49b274d6949a70
Loupi <louis_pierre_beaumont@hotmail.com>
2018-06-03 00:35:29 -0400

    Revert to manual build of libcypher-parser.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,9 @@
-add-apt-repository ppa:cleishm/neo4j
-apt-get update
-apt-get install libcypher-parser
+wget https://github.com/cleishm/libcypher-parser/releases/download/v0.6.0/libcypher-parser-0.6.0.tar.gz \
+&& tar zxvpf libcypher-parser-0.6.0.tar.gz \
+&& rm libcypher-parser-0.6.0.tar.gz \
+&& cd libcypher-parser-0.6.0 \
+&& ./configure --prefix=/usr \
+&& make clean check \
+&& sudo make install \
+&& cd .. \
+&& rm -rf libcypher-parser-0.6.0
