b37391175994dac543867701af337fbd409b3a86
Loupi <louis_pierre_beaumont@hotmail.com>
2018-06-02 23:52:56 -0400

    Add custom install step in travis for libcypher-parser.

---
+++ install-dependencies.sh
@@ -0,0 +1,9 @@
+wget https://github.com/cleishm/libcypher-parser/releases/download/v0.6.0/libcypher-parser-0.6.0.tar.gz \
+&& tar zxvpf libcypher-parser-0.6.0.tar.gz \
+&& rm libcypher-parser-0.6.0.tar.gz \
+&& cd libcypher-parser-0.6.0 \
+&& ./configure --prefix=/usr \
+&& make clean check \
+&& make install \
+&& cd .. \
+&& rm -rf libcypher-parser-0.6.0
