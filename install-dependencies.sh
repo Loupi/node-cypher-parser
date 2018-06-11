83d78cd8f09d6277692d906391fb7e153b354e03
Loupi <louis_pierre_beaumont@hotmail.com>
2018-06-11 18:26:04 -0400

    Pass CFLAGS and LDFLAGS manually.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,16 @@
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+wget https://github.com/cleishm/libcypher-parser/releases/download/v0.6.0/libcypher-parser-0.6.0.tar.gz \
+&& tar zxvpf libcypher-parser-0.6.0.tar.gz \
+&& rm libcypher-parser-0.6.0.tar.gz \
+&& cd libcypher-parser-0.6.0 \
+&& ./configure --prefix=/usr/local CFLAGS='-fPIC -mmacosx-version-min=10.7' LDFLAGS='-mmacosx-version-min=10.7' \
+&& make clean check \
+&& sudo make install \
+&& cd .. \
+&& rm -rf libcypher-parser-0.6.0
+fi
+
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 wget https://github.com/cleishm/libcypher-parser/releases/download/v0.6.0/libcypher-parser-0.6.0.tar.gz \
 && tar zxvpf libcypher-parser-0.6.0.tar.gz \
 && rm libcypher-parser-0.6.0.tar.gz \
@@ -7,3 +20,4 @@ wget https://github.com/cleishm/libcypher-parser/releases/download/v0.6.0/libcyp
 && sudo make install \
 && cd .. \
 && rm -rf libcypher-parser-0.6.0
+fi
