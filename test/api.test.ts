b9ea7deedfa8fc19824748bc5339c441b8c9fae0
Loupi <louis_pierre_beaumont@hotmail.com>
2018-06-11 19:59:14 -0400

    Set dumpAst to true in test.

--- test/api.test.ts
+++ test/api.test.ts
@@ -7,8 +7,8 @@ const query = "MATCH (node1:Label1)-->(node2:Label2)\n" +
               "RETURN node2.propertyA, node2.propertyB";
 
 describe("parse query", () => {
-  it("should return a valid result", async () => {
-    const result = await cypher.parse(query);
+  it("should return a valid parse result", async () => {
+    const result = await cypher.parse({query, dumpAst: true});
     expect(result).to.be.an("object");
     expect(result).to.have.property("ast");
     expect(result.ast).to.be.a("string");
