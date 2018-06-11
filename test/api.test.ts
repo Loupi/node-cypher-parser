3437fcc30f23595bb7df6a7073cb2b818b98028d
Loupi <louis_pierre_beaumont@hotmail.com>
2018-06-11 19:23:45 -0400

    Add mocha and chai, create first test.

---
+++ test/api.test.ts
@@ -0,0 +1,19 @@
+import { expect } from "chai";
+import * as cypher from "../lib/index";
+
+const query = "MATCH (node1:Label1)-->(node2:Label2)\n" +
+              "WHERE node1.propertyA = {value}\n" +
+              "RETURNI node2.propertyA, node2.propertyB";
+
+describe("parse query", () => {
+  it("should return a valid result", async () => {
+    const result = await cypher.parse(query);
+    expect(result).to.be.an("object");
+    expect(result).to.have.property("ast");
+    expect(result.ast).to.be.a("string");
+    expect(result).to.have.property("nnodes");
+    expect(result.nnodes).to.be.a("number");
+    expect(result).to.have.property("roots");
+    expect(result.roots).to.be.an("array");
+  });
+});
